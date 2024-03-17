c10896c116ed9281223d220243929daf4b4d6173
TheNeon001 <163774565+TheNeon001@users.noreply.github.com>
2024-03-17 23:31:11 +0300

    Update Рабочий. Вывод в бот без имени отправителя.py

--- Рабочий. Вывод в бот без имени отправителя.py
+++ Рабочий. Вывод в бот без имени отправителя.py
@@ -1,10 +1,10 @@
 from telethon import TelegramClient, events
 from telegram import Bot
 
-api_id = 25270283
-api_hash = "e185dbd4bd50b6a1f5640a0a51c8b966"
-phone = "+79274523107"
-bot_token = "7128578861:AAGq_MSwAo-YCcptQgOmuQDY8SaueKlkuxc"  # Вставьте сюда токен вашего бота
+api_id = 00000000
+api_hash = "e185dbd4bd50b6a1f5640a0a50000000"
+phone = "+0000000000"
+bot_token = "7128578861:AAGq_MSwAo-YCcptQgOmuQD00000000000"  # Вставьте сюда токен вашего бота
 
 # Создаем клиент Telegram
 client = TelegramClient('session_name', api_id, api_hash)
